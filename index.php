772e7cf43c0a1533b5c8a904092aef3ae15990dc
huajinshang <2430960358@qq.com>
2017-07-12 11:36:29 +0800

    h

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header("content-type:text/html;charset=utf-8");
 define("COMING","yes");
 $server=$_SERVER;
 define("ROOT_PATH",$server["DOCUMENT_ROOT"]);
-define("APP_PATH",ROOT_PATH."/MVC/lvyou");
+define("APP_PATH",ROOT_PATH."/travel");
 
 define("LIBS_PATH",APP_PATH."/libs");
 define("TPL_PATH",APP_PATH."/template");
